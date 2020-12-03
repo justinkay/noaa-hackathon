25bd245b5d9564abbd4631e7252e54ed85765f2c
justinkay <justinkay92@gmail.com>
2020-12-03 18:07:11 +0000

    Made pip installabe.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='hackathon',
+    packages=find_packages(),
+    version='',
+    description='',
+    author='justinkay',
+    license='',
+)
